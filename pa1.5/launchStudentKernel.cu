b245464901fbefda0ec4b8571d12a5967e3111d0
Sailesh <iampsylesh13@gmail.com>
2026-03-06 19:38:25 -0500

    created main.cu, added timing, trying optimization for tensor code

--- pa1.5/launchStudentKernel.cu
+++ pa1.5/launchStudentKernel.cu
@@ -12,8 +12,7 @@
 // A is row major, B is column major, C is row major
 // A, B and C are pointers to the matrices
 
-__global__ void tensorcore_gemm(__half *A, __half *B, float *C, int M, int N, int K){
-
+__global__ void tensorcore_gemm(__half *A, __half *B, float *C, int M, int N, int K) {
     int warpID  = threadIdx.x / 32;
     int laneID  = threadIdx.x % 32;
 
@@ -23,58 +22,60 @@ __global__ void tensorcore_gemm(__half *A, __half *B, float *C, int M, int N, in
     int out_row = blockIdx.y * 32 + warp_row * 16;
     int out_col = blockIdx.x * 32 + warp_col * 8;
 
-    __shared__ __half A_shared[8*256];
-    __shared__ __half B_shared[8*128];
-    __shared__ float  C_shared[8*128];
+    __shared__ __half A_shared[2 * 256];
+    __shared__ __half B_shared[4 * 128];
+    __shared__ float  C_shared[8 * 128];
 
-    // Each warp gets a pointer to its own slice
-    __half *warp_A = A_shared + warpID * 256;
-    __half *warp_B = B_shared + warpID * 128;
+    __half *warp_A = A_shared + warp_row * 256;
+    __half *warp_B = B_shared + warp_col * 128;
     float  *warp_C = C_shared + warpID * 128;
 
-    // Initialize C_shared to 0
     for (int i = laneID; i < 128; i += 32)
         warp_C[i] = 0.0f;
     __syncthreads();
 
-    // K loop
     for (int iter = 0; iter < K/16; iter++) {
 
-        // Load A (row-major global → row-major shared)
+        int k_base = iter * 16;
+
+        if (warp_col == 0){
+        #pragma unroll
         for (int i = laneID; i < 256; i += 32) {
             int row = i / 16;
             int col = i % 16;
-            int gr  = out_row + row;
-            int gk  = iter * 16 + col;
-            warp_A[i] = (gr < M && gk < K) ? A[gr * K + gk] : __float2half(0.f);
-        }
 
-        // Load B (row-major global → col-major shared)
+            int gr = out_row + row;
+            int gk = k_base + col;
+
+            A_shared[warp_row * 256 + i] = A[gr * K + gk];
+            }
+        }
+        if (warp_row == 0){
+        #pragma unroll
         for (int i = laneID; i < 128; i += 32) {
             int col = i / 16;
             int row = i % 16;
-            int gk  = iter * 16 + row;
-            int gn  = out_col + col;
-            // Column-major global B: B(k,n) at k + n*K
-            warp_B[col * 16 + row] =
-            (gk < K && gn < N) ? B[gk + gn * K] : __float2half(0.f);
-          }
 
+            int gk = k_base + row;
+            int gn = out_col + col;
+
+            B_shared[warp_col * 128 + col * 16 + row] = B[gk + gn * K];;
+        }
+    }
         __syncthreads();
         mma_m16n8k16_f16_f16_smem_row_col(warp_A, warp_B, warp_C);
         __syncthreads();
-    }
+        }
 
-    // Store C back to global
-    for (int i = laneID; i < 128; i += 32) {
-        int row = i / 8;
-        int col = i % 8;
-        int gr  = out_row + row;
-        int gn  = out_col + col;
-        if (gr < M && gn < N)
-            C[gr * N + gn] = warp_C[i];
+        for (int i = laneID; i < 128; i += 32) {
+            int row = i / 8;
+            int col = i % 8;
+            int gr  = out_row + row;
+            int gn  = out_col + col;
+            if (gr < M && gn < N)
+                C[gr * N + gn] = warp_C[i];
+        }
     }
-}
 
 void launchStudentKernel(int M, int N, int K, __half* A,
                                               __half* B,
